76a3d4e327c4c3321ea96d7676fb5b7d3d0a0425
xiraynedev <xiraynedev@gmail.com>
2020-03-13 10:01:02 -0400

    moving key into class

--- weather.js
+++ weather.js
@@ -1,8 +1,6 @@
-const key = config.MY_KEY;
-
 class Weather {
   constructor() {
-    
+    this.key = config.MY_KEY;
     this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
     this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   }
